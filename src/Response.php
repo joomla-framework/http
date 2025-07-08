aa3bcb72ee5d7a5ddf52d514ba18d6b588b1fe51
Hannes Papenberg <info@joomlager.de>
2025-07-08 21:30:36 +0200

    Removing deprecated magic getter

--- src/Response.php
+++ src/Response.php
@@ -14,50 +14,8 @@ use Laminas\Diactoros\Response as PsrResponse;
 /**
  * HTTP response data object class.
  *
- * @property-read  string   $body     The response body as a string
- * @property-read  integer  $code     The status code of the response
- * @property-read  array    $headers  The headers as an array
- *
  * @since  1.0
  */
 class Response extends PsrResponse
 {
-    /**
-     * Magic getter for backward compatibility with the 1.x API
-     *
-     * @param   string  $name  The variable to return
-     *
-     * @return  mixed
-     *
-     * @since   2.0.0
-     * @deprecated  3.0  Access data via the PSR-7 ResponseInterface instead
-     */
-    public function __get($name)
-    {
-        switch (strtolower($name)) {
-            case 'body':
-                return (string) $this->getBody();
-
-            case 'code':
-                return $this->getStatusCode();
-
-            case 'headers':
-                return $this->getHeaders();
-
-            default:
-                $trace = debug_backtrace();
-
-                trigger_error(
-                    sprintf(
-                        'Undefined property via __get(): %s in %s on line %s',
-                        $name,
-                        $trace[0]['file'],
-                        $trace[0]['line']
-                    ),
-                    E_USER_NOTICE
-                );
-
-                break;
-        }
-    }
 }
